ff9eed989972759d83cf5399f017b5666227ccf2
Antoine Cadoret <antoinecadoret97@gmail.com>
2023-08-18 15:36:47 +0200

    Build (AlyreScore) add review bars

--- src/components/molecules/AlyreScore/AlyreScore.tsx
+++ src/components/molecules/AlyreScore/AlyreScore.tsx
@@ -1,13 +1,27 @@
 const AlyreScore = () => {
+    interface starInterface {
+        id: string;
+        purcentage: number;
+    }
+    const starReviews: Array<starInterface> = [
+        { id: '5', purcentage: 74 },
+        { id: '4', purcentage: 54 },
+        { id: '3', purcentage: 24 },
+        { id: '2', purcentage: 5 },
+        { id: '1', purcentage: 14 },
+    ];
     return (
-        <div className="rounded-md border-4 w-4/5 m-5">
+        <div className="flex flex-col items-center rounded-md border-4 w-4/5 m-5">
             <h3>Alyre Score</h3>
-            <div>
-                <p>5 stars</p>
-                <p>4 stars</p>
-                <p>3 stars</p>
-                <p>2 stars</p>
-                <p>1 stars</p>
+            <div className="flex flex-col items-center w-4/5">
+                {starReviews.map((review, index) => {
+                    return <div key={index} className="w-full">
+                        <p>{review.id} stars</p>
+                        <div className="review-container rounded-md bg-slate-600 h-3 p-0 mb-5">
+                            <div className="star-quantity"></div>
+                        </div>
+                    </div>
+                })}
             </div>
         </div>
     );
