36120e55f583967483f30cb69f5aaa9350be9994
Antoine Cadoret <antoinecadoret97@gmail.com>
2023-08-21 15:40:07 +0200

    Fix(Book Cover) put the color at the right place in the quick review

--- src/components/molecules/BookCover/BookCover.tsx
+++ src/components/molecules/BookCover/BookCover.tsx
@@ -8,45 +8,50 @@ import list from '../../../assets/library.png'
 import { useState } from "react";
 
 interface BookCoverInterface extends GlobalChildren {
+    // key: any;
+    id: string;
     imageUrl: string;
     imageWidth: string;
     imageHeigth: string;
     recommended: boolean;
 }
 
-const BookCover = ({ imageUrl, imageHeigth, imageWidth, recommended }: BookCoverInterface) => {
+const BookCover = ({ id, imageUrl, imageHeigth, imageWidth, recommended }: BookCoverInterface) => {
     const elementImg = { book: { closed: bookClose, open: bookOpen } }
     const [bookImg, setBookImg] = useState(bookClose);
     const [heartImg, setHeartImg] = useState(notLikedHeart);
     const [readListImg, setReadListImg] = useState(list);
 
-    const toggleButton = (sectionId: string) => {
-        const element = document.getElementById(sectionId);
+    const toggleButton = (event: any) => {
+        const element = event.currentTarget;
+
         const elementClassList = element!.classList;
 
-        if (element?.className.includes('closed')) {
+        if (element?.classList.contains('closed')) {
             element!.style.color = "#2BBDE1";
             elementClassList.add('open');
             elementClassList.remove('closed');
-            switch (element!.id) {
-                case ('book'): setBookImg(bookOpen);
-                    break;
-                case ('heart'): setHeartImg(likedHeart);
-                    break;
-                case ('readList'): setReadListImg(addedToList);
-                    break;
+            if (element!.id.includes('book')) {
+                setBookImg(bookOpen);
+            }
+            if (element!.id.includes('heart')) {
+                setHeartImg(likedHeart);
+            }
+            if (element!.id.includes('readList')) {
+                setReadListImg(addedToList);
             }
         } else {
             element!.style.color = "#FFFFFF";
             elementClassList.remove('open');
             elementClassList.add('closed');
-            switch (element!.id) {
-                case ('book'): setBookImg(bookClose);
-                    break;
-                case ('heart'): setHeartImg(notLikedHeart);
-                    break;
-                case ('readList'): setReadListImg(list);
-                    break;
+            if (element!.id.includes('book')) {
+                setBookImg(bookClose);
+            }
+            if (element!.id.includes('heart')) {
+                setHeartImg(notLikedHeart);
+            }
+            if (element!.id.includes('readList')) {
+                setReadListImg(list);
             }
         }
     };
@@ -54,15 +59,15 @@ const BookCover = ({ imageUrl, imageHeigth, imageWidth, recommended }: BookCover
     return (
         <div className="relative rounded-lg" style={{ height: imageHeigth, flexBasis: recommended ? '17%' : 'auto', backgroundImage: `url(${imageUrl})`, backgroundSize: "cover", backgroundPosition: "center", backgroundRepeat: 'no-repeat', width: '50%' }}>
             <div className="flex justify-around absolute rounded-lg inset-x-0 bottom-0 bg-slate-600 opacity-80 h-32">
-                <button id="book" className="closed flex flex-col items-center justify-center" onClick={() => toggleButton('book')}>
+                <button id={`book-${id}`} className="closed flex flex-col items-center justify-center" onClick={toggleButton.bind(this)}>
                     <img src={bookImg} alt="closed book" />
                     <p>read</p>
                 </button>
-                <button id="heart" className="closed flex flex-col items-center justify-center" onClick={() => toggleButton('heart')}>
+                <button id={"heart-" + id} className="closed flex flex-col items-center justify-center" onClick={toggleButton.bind(this)}>
                     <img src={heartImg} alt="closed heart" />
                     <p>like</p>
                 </button>
-                <button id="readList" className="closed flex flex-col items-center justify-center" onClick={() => toggleButton('readList')}>
+                <button id={"readList-" + id} className="closed flex flex-col items-center justify-center" onClick={toggleButton.bind(this)}>
                     <img src={readListImg} alt="closed read list" />
                     <p>read list</p>
                 </button>
