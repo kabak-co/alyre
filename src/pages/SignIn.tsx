11dc190d9eb2690fe79975137cdf2b3ff9f29bb7
Antoine Cadoret <antoinecadoret97@gmail.com>
2023-08-31 17:41:35 +0200

    Build(Sign In) sign in page skeleton

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,10 +1,18 @@
 import { Link } from "react-router-dom";
 
 const SignIn = () => {
+
     return (
         <div>
-            <h1>Sign In</h1>
-            <Link to={'/signup'}>Sign Up</Link>
+            <h1 className="m-6 text-4xl">Sign In</h1>
+            <p className="m-6">You do not have an Alyre account? <Link className="underline text-cyan-600" to={'/signup'}>Sign Up</Link></p>
+            <div className="flex flex-col">
+                <label htmlFor="email">Email:</label>
+                <input className="my-4 text-slate-500" type="text" name="email" id="email" />
+                <label htmlFor="password">Password:</label>
+                <input className="my-4 text-slate-500" type="text" name="password" id="password" />
+                <button className="my-6 bg-cyan-600 rounded-md">Sign In</button>
+            </div>
         </div>
     );
 };
