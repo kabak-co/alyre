8ef5c42035bdb00738c6e1ddbc7251da9cf40274
Antoine Cadoret <antoinecadoret97@gmail.com>
2023-09-01 19:00:16 +0200

    Build(SignIn) input validation

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,7 +1,45 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 const SignIn = () => {
+    const [isError, setIsError] = useState<boolean>(false);
+    const [errorMessage, setErrorMessage] = useState<Array<string>>([]);
 
+    const verifyData = (value: string, regex: RegExp): boolean => {
+        return regex.test(value);
+    };
+
+    const logIn = () => {
+        const emailElement = document.getElementById('email') as HTMLInputElement;
+        const passwordElement = document.getElementById('password') as HTMLInputElement;
+
+        let errorMessages: Array<string> = [];
+
+        const data = {
+            email: emailElement.value,
+            password: passwordElement.value
+        };
+
+        if (!data.email) {
+            emailElement.style.border = '0.3rem solid red';
+            errorMessages.push('You have to enter an email');
+        } else {
+            if (!verifyData(data.email, /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/)) {
+                emailElement.style.border = '0.3rem solid red';
+                errorMessages.push('The email is invalid');
+            }
+        }
+        if (!data.password) {
+            emailElement.style.border = '0.3rem solid red';
+            errorMessages.push('You have to enter a password');
+        } else {
+            if (!verifyData(data.password, /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,20}$/)) {
+                passwordElement.style.border = '0.3rem solid red';
+                errorMessages.push('The password is invalid');
+            }
+        }
+
+    };
     return (
         <div>
             <h1 className="m-6 text-4xl">Sign In</h1>
